3cd196f1bbb9443b7a4b1edc4f8ad323da78d47b
Eric Freeman <eric.freeman@extendhealth.com>
2014-03-20 23:09:31 -0600

    okay, now i think everything's fixed

--- Assets/Resources/CloudScript.cs
+++ Assets/Resources/CloudScript.cs
@@ -17,15 +17,11 @@ public class CloudScript : MonoBehaviour
 	    for (int i = 0; i < CloudCount; i++)
 	    {
 	        _objs.Add(BackgroundObject.Create(CloudTexture, 0f, 6f, .01f, .03f, .9f, .7f));
-	        var cloud = new GameObject();
-	        cloud.AddComponent<SpriteRenderer>();
-	        var s = cloud.GetComponent<SpriteRenderer>();
-	        s.sprite = CloudTexture;
 	    }
 
 	    for (int i = 0; i < BuildingCount; i++)
 	    {
-	        _objs.Add(BackgroundObject.Create(BuildingTexture, -.85f, -.85f, .005f, .01f, .8f));
+	        _objs.Add(BackgroundObject.Create(BuildingTexture, -.85f, -.85f, .005f, .01f, .7f));
 	    }
 	}
 	
