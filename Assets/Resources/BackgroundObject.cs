9469634f143ae974c0d3a08d2fe1bf6d634926f6
Eric Freeman <eric.freeman@extendhealth.com>
2014-03-20 22:57:14 -0600

    more refactors and added buildings to background as well

---
+++ Assets/Resources/BackgroundObject.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Assets.Resources
+{
+    public class BackgroundObject
+    {
+        public GameObject GameObject;
+        public float MoveSpeed;
+
+        public float MaxHeight;
+        public float MinHeight;
+
+        public float MinScale;
+
+        private float _z;
+
+        public static BackgroundObject Create(Sprite sprite, float minHeight, float maxHeight, float minSpeed, float maxSpeed, float z, float minScale = 1f)
+        {
+            var cloudObj = new BackgroundObject {GameObject = new GameObject()};
+            cloudObj.GameObject.AddComponent("SpriteRenderer");
+            cloudObj.MaxHeight = maxHeight;
+            cloudObj.MinHeight = minHeight;
+            cloudObj._z = z;
+            cloudObj.MinScale = minScale;
+            var s = cloudObj.GameObject.GetComponent<SpriteRenderer>();
+            s.sprite = sprite;
+
+            s.transform.position = new Vector3(Random.Range(-10, 10), Random.Range(minHeight, maxHeight), 1f);
+            s.transform.localScale = new Vector3(Random.Range(minScale, 1f), Random.Range(minScale, 1f), Random.Range(minScale, 1f));
+
+            cloudObj.MoveSpeed = Random.Range(minSpeed, maxSpeed);
+
+            return cloudObj;
+        }
+
+        public void Update()
+        {
+           GameObject.transform.position += Vector3.left * MoveSpeed;
+
+           if (GameObject.transform.position.x < -11.5)
+            {
+                GameObject.transform.position = new Vector3(Random.Range(10.5f, 12), Random.Range(MinHeight, MaxHeight), 1f);
+                GameObject.transform.localScale = new Vector3(Random.Range(MinScale, 1f), Random.Range(MinScale, 1f), Random.Range(MinScale, 1f));
+                MoveSpeed = Random.Range(.01f, .03f);
+            }
+        }
+    }
+}
--- Assets/Resources/CloudScript.cs
+++ Assets/Resources/CloudScript.cs
@@ -1,44 +1,36 @@
 ﻿using System.Collections.Generic;
 using System.Runtime.Remoting.Messaging;
+using Assets.Resources;
 using UnityEngine;
 
 public class CloudScript : MonoBehaviour
 {
     public Sprite CloudTexture;
-    public int CloudCount = 5;
+    public Sprite BuildingTexture;
 
-    private List<GameObject> _clouds = new List<GameObject>();
-    private List<float> _cloudSpeeds = new List<float>(); //yes I know this is a terrible hack, I'm sorry :( 
+    public int CloudCount = 5;
+    public int BuildingCount = 3;
+
+    private List<BackgroundObject> _objs = new List<BackgroundObject>();
 
 	// Use this for initialization
 	void Start () {
 	    for (int i = 0; i < CloudCount; i++)
 	    {
-	        var cloud = new GameObject();
-	        cloud.AddComponent("SpriteRenderer");
-	        var s = (SpriteRenderer)cloud.GetComponent("SpriteRenderer");
-	        s.sprite = CloudTexture;
+            _objs.Add(BackgroundObject.Create(CloudTexture, 0f, 6f, .01f, .03f, .9f, .7f));
+	    }
 
-            s.transform.position = new Vector3(Random.Range(-10, 10), Random.Range(0, 6), 1f);
-            s.transform.localScale = new Vector3(Random.Range(.7f, 1f), Random.Range(.7f, 1f), Random.Range(.7f, 1f));
-
-            _clouds.Add(cloud);
-            _cloudSpeeds.Add(Random.Range(.01f, .03f));
+	    for (int i = 0; i < BuildingCount; i++)
+	    {
+	        _objs.Add(BackgroundObject.Create(BuildingTexture, -.85f, -.85f, .005f, .01f, .8f));
 	    }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        for (int i = 0; i < CloudCount; i++)
-        {
-            _clouds[i].transform.position -= new Vector3(_cloudSpeeds[i], 0f, 0f);
-
-            if (_clouds[i].transform.position.x < -10)
-            {
-                _clouds[i].transform.position = new Vector3(Random.Range(10.5f, 12), Random.Range(0, 6), 1f);
-                _clouds[i].transform.localScale = new Vector3(Random.Range(.7f, 1f), Random.Range(.7f, 1f), Random.Range(.7f, 1f));
-                _cloudSpeeds[i] = Random.Range(.01f, .03f);
-            }
+        foreach (var obj in _objs)
+	    {
+	        obj.Update();
 	    }
 	}
 }
